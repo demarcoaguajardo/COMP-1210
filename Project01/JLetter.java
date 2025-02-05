92256a0fe79b42e9f7ac157b738c1f41a76b07af
Demarco Guajardo <124603625+demarcoaguajardo@users.noreply.github.com>
2025-02-05 11:05:20 -0600

    Add files via upload

---
+++ Project01/JLetter.java
@@ -0,0 +1,31 @@
+   /**
+   Program to create the block letter, "J", using
+   only these characters: "J", "A", "V", and "A". 
+   Intro to Java Project B
+   Demarco Guajardo - COMP 1210 - 001
+   08/20/22
+   */
+   
+public class JLetter
+{
+
+/**
+   Prints the block letter "J" with met conditions.
+   @param args Command line arguments - not used.
+   */
+   
+   public static void main(String[] args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA");
+      System.out.println("  JAVAJA");
+         
+   }  
+}
---
+++ Project01/MyLifeGoals.java
@@ -0,0 +1,30 @@
+/** 
+   Simple program to describe my life goals. 
+   Intro to Java Project A
+   Demarco Guajardo - COMP 1210 - 001
+   08/20/22
+   */ 
+      
+public class MyLifeGoals
+{
+      
+/**
+      Prints my name along with my short-term, medium-term,
+      and long-term life goals. 
+      @param args Command line arguments - not used.
+      */ 
+      
+   public static void main(String[] args)
+   {
+      System.out.println("Demarco Guajardo \n");
+      System.out.println("One of my short-term goals is to be able to "
+         + "learn how to play billiards since it is fun and challenging. "
+         + "Activities that are challenging are satisfying to learn.");
+      System.out.println("One of my medium-term goals is to be able "
+         + "to save enough money to buy myself a new car since the "
+         + "one I have is on its last breath.");
+      System.out.println("One of my long-term goals is to be able "
+         + "to make a successful living off a career in "
+         + "Software Engineering so that I can pay off all my student debt.");
+   }
+}
